0e7a86ee41724771659641b4c64e1f89add645fc
Đỗ Phúc Đạt <129186667+DOPHUCDAT@users.noreply.github.com>
2025-05-23 15:02:32 +0700

    Update DAG.py

--- dags/DAG.py
+++ dags/DAG.py
@@ -8,30 +8,31 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 #fetch data 
-
 header = {
-    "Referer" : "https://www.amazon.com/",
+    "Referer" : "https://www.amazon.com/ref=nav_logo",
     "Sec-Ch-Ua": "Microsoft Edge",
     "Sec-Ch-Ua-Platform": "Windows",
     "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0"
 }
 #Extract and Transform
 def get_item(num, ti):
-    base_url = f"https://www.amazon.com/s?k=elden+ring&crid=KS5QRMFGXDJD&sprefix=elden+ring+%2Caps%2C319&ref=nb_sb_noss_2"
+    base_url = f"https://www.amazon.com/s?k=elden+ring&crid=3HAM01VJPAK40&sprefix=%2Caps%2C367&ref=nb_sb_ss_recent_1_0_recent"
     items = []
     #Lưu lại tiêu đề đã xem vào trong kiểu set để tránh duplicate
     seen_title = set()  
     page = 1
+    session = requests.Session()
+    session.headers.update(header)
     while len(items) < num:
         url = f"{base_url}&page={page}"
-        response = requests.get(url, headers=header)
+        response = session.get(url)
         #Mã check xem yêu cầu có thành công không (200 là ok, 404 not found)
         if response.status_code == 200:  
             soup = BeautifulSoup(response.content, "html.parser")
-            item_containers = soup.find_all("div",{"class": "s-result-item"})
+            item_containers = soup.find_all("div",{"class": "sg-col-inner"})
 
             for item in item_containers:
-                title = item.find("span",{"class" : "a-size-base-plus a-color-base a-text-normal"})
+                title = item.find("a",{"class" : "a-link-normal s-line-clamp-4 s-link-style a-text-normal"})
                 price = item.find("span",{"class" : "a-price-whole"})
                 rating = item.find("span",{"class" : "a-icon-alt"})
                 if title and price and rating:
@@ -75,16 +76,17 @@ def Load_into_postgres(ti):
 default_args = {
     'owner' : 'airflow',
     'depends_on_past' : False,
-    'start_date' : datetime(2024,10,2),
+    'start_date' : datetime(2024,2,18),
     'retries' : 1,
     'retry_delay' : timedelta(minutes=5)
 }
 
 dag = DAG(
-    'first_pipeline',
+    'ETL_pipeline',
     default_args=default_args,
     description='A simple DAG to extract item data from Amazon and store it in Postgres',
-    schedule=timedelta(days=1)
+    schedule_interval='@daily',
+    catchup= False
 )   
 
 extract_data_task = PythonOperator(
@@ -115,8 +117,4 @@ insert_data_task = PythonOperator(
 )
 
 extract_data_task >> create_table_task >> insert_data_task
-
-
-            
-
-
+ 
